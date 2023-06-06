078d8d90ec0a5445bdff777feb773aeda5ff91f2
Anthony Ramirez <tonyjo.ramirez@gmail.com>
2023-06-06 13:06:43 -0400

    form and sell creation

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header(){
+    return(
+        <header>
+            <img src="{/*some JS variable*/}" placeholder="https://i.pinimg.com/originals/b2/81/1e/b2811e62664abf38513dcfa125936e03.jpg" className="App-logo" alt="logo" />
+            <p>Autobots Toyota</p>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Sell.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header"
+import Form from "./Form"
+
+function Sell(){
+    return(
+        <div>
+            {/*<Header />*/}
+            <Form />
+        </div>
+    )
+}
+
+export default Sell
